7f70339c0848d2588324dccdfafbfeff8f7b3388
allisondo <phuongthaodo02042003@gmail.com>
2025-05-13 22:01:53 +0700

    update

--- app/src/main/java/com/example/news/NewsApi.java
+++ app/src/main/java/com/example/news/NewsApi.java
@@ -1,42 +1,25 @@
-package com.example.fastnews;
+package com.example.news;
 
 import retrofit2.Call;
-import retrofit2.Retrofit;
-import retrofit2.converter.gson.GsonConverterFactory;
 import retrofit2.http.GET;
 import retrofit2.http.Query;
-import java.util.List;
 
-public class NewsApi {
-    private static NewsApi instance;
-    private final NewsApiService newsApiService;
-
-    private NewsApi() {
-        Retrofit retrofit = new Retrofit.Builder()
-                .baseUrl("https://gnews.io/api/v4/")
-                .addConverterFactory(GsonConverterFactory.create())
-                .build();
-        newsApiService = retrofit.create(NewsApiService.class);
-    }
-
-    public static NewsApi getInstance() {
-        if (instance == null) {
-            instance = new NewsApi();
-        }
-        return instance;
-    }
-
-    public NewsApiService getNewsService() {
-        return newsApiService;
-    }
-}
-
-interface NewsApiService {
-    @GET("search")
-    Call<List<NewsModel>> searchNews(
-            @Query("q") String query,
+public interface NewsApi {
+    @GET("top-headlines")
+    Call<NewsModel> getNewsByCategory(
+            @Query("token") String apiKey,
+            @Query("category") String category,
             @Query("lang") String language,
             @Query("country") String country,
-            @Query("apikey") String apiKey
+            @Query("max") int max
+    );
+
+    @GET("search")
+    Call<NewsModel> getNewsByKeywords(
+            @Query("token") String apiKey,
+            @Query("q") String keywords,
+            @Query("lang") String language,
+            @Query("country") String country,
+            @Query("max") int max
     );
 }
