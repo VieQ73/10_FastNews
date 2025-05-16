b19f0884ca4c57192013a13625ebfdde312405ee
VieQ73 <fanbox2003@gmail.com>
2025-05-16 19:17:52 +0700

    Fix NewsAdapter

--- app/src/main/java/com/example/news/adapters/NewsAdapter.java
+++ app/src/main/java/com/example/news/adapters/NewsAdapter.java
@@ -1,4 +1,4 @@
-package com.example.news;
+package com.example.news.adapters;
 
 import android.content.Context;
 import android.content.Intent;
@@ -8,6 +8,8 @@ import android.view.ViewGroup;
 
 import androidx.annotation.NonNull;
 import androidx.recyclerview.widget.RecyclerView;
+
+import com.example.news.R;
 
 import java.util.List;
 
